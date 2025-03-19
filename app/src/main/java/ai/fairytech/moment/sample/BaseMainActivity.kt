2de9b7b9fd62eab0a69845b31fd77a9548b73a0d
kasel-fairy <kasel@fairytech.ai>
2025-03-19 18:02:20 +0900

    refactor: remove commented-out code in BaseMainActivity

--- app/src/main/java/ai/fairytech/moment/sample/BaseMainActivity.kt
+++ app/src/main/java/ai/fairytech/moment/sample/BaseMainActivity.kt
@@ -35,11 +35,5 @@ open class BaseMainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-//        setSupportActionBar(binding.toolbar)
-
-//        val navController = findNavController(R.id.nav_host_fragment_content_my_page)
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
